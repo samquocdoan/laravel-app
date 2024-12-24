b21c5543be30162c1ec3e3fec840b43dae0d37d7
samquocdoan <samquocdoan.me@gmail.com>
2024-12-24 16:13:33 +0700

    add about and contact views with forms and update layout file

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container mx-auto px-4 py-8">
+        <h1 class="text-4xl font-bold mb-4">About Us</h1>
+        <p class="mb-4">
+            Welcome to our website! We are dedicated to providing you with the best service possible.
+        </p>
+        <p class="mb-4">
+            Our team is composed of experienced professionals who are passionate about what they do.
+        </p>
+        <p class="mb-4">
+            We believe in continuous improvement and always strive to exceed our customers' expectations.
+        </p>
+        <p>
+            Thank you for visiting our site. We look forward to serving you!
+        </p>
+    </div>
+@endsection
