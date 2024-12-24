07eb7c164768377410e7e4b03eb47e4a3bcb7478
samquocdoan <samquocdoan.me@gmail.com>
2024-12-24 15:41:06 +0700

    remove JS and Vue file paths from Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
