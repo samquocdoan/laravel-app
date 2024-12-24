c8565760cba276210574ceeaaf19639875bd3fda
samquocdoan <samquocdoan.me@gmail.com>
2024-12-24 16:02:43 +0700

    add PagesController and update routes for index, about, and contact views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PagesController::class, 'index']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/contact', [PagesController::class, 'contact']);
