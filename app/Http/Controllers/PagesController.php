07a79d8135fb952e6384b48bbfa9fcf133bdc3c8
samquocdoan <samquocdoan.me@gmail.com>
2024-12-27 16:22:37 +0700

    fetch posts from database and pass to index view

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 class PagesController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $posts = DB::table('posts')
+        ->join('users', 'posts.user_id', '=', 'users.id')
+        ->select('posts.*', 'users.name as user_name')
+        ->get();
+        return view('index', compact('posts'));
     }
 
     public function about()
