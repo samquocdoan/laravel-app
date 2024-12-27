19ae0897f5df83815bb02eef8a2c671f532dd231
samquocdoan <samquocdoan.me@gmail.com>
2024-12-27 16:23:18 +0700

    add route for posts index page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
 Route::get('/contact', [PagesController::class, 'contact']);
+Route::get('/posts', [PostController::class, 'index']);
